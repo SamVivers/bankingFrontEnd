0dd4a8d16d4345b40f07f366345e0aec2560b013
SamVivers <viverssamuel@gmail.com>
2019-05-24 15:05:13 +0100

    updated rerender

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 export default class App extends Component {
   constructor(props) {
@@ -19,11 +22,18 @@ export default class App extends Component {
     if (this.state.id === 0) {
       return (
         <div>
-          <div>
-            <input placeholder="Account ID" onChange={this.inputId}></input>
-            <button onClick={this.getAcc}>Login</button>
-            <br />
-            <button onClick={this.createAcc}>New Account</button>
+          <div class="jumbotron">
+            <h1 class="display-4">Welcome to QA Bank!</h1>
+            <p class="lead">To access your account, please enter your id below.</p>
+            <hr class="my-4" />
+            <div>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Account ID" onChange={this.inputId}></input>
+                <div class="input-group-append">
+                  <button type="button" class="btn btn-outline-secondary" onClick={this.getAcc}>Login</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       );
@@ -49,148 +59,150 @@ export default class App extends Component {
     }
   }
 
-    getAcc = () => {
-      let URL = `/api/account/` + this.state.inputId;
-      let request = new XMLHttpRequest();
-      request.open('GET', URL);
-      request.responseType = 'json';
-      request.onload = () => {
+  getAcc = () => {
+    let URL = `/api/account/` + this.state.inputId;
+    let request = new XMLHttpRequest();
+    request.open('GET', URL);
+    request.responseType = 'json';
+    request.onload = () => {
+      this.setState({
+        id: request.response.id,
+        balance: request.response.balance
+      });
+    }
+    request.send();
+  }
+
+  createAcc = () => {
+    let URL = `/api/account`;
+    let request = new XMLHttpRequest();
+    request.open('POST', URL);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Accept", "application/json");
+    request.responseType = 'json';
+    request.onload = () => {
+      console.log(request.response)
+      this.setState({
+        id:request.response.id,
+      })
+    }
+    let body = {
+      balance: 0
+    };
+    body = JSON.stringify(body);
+    request.send(body);
+  }
+
+  deleteAcc = () => {
+    let URL = `/api/account/` + this.state.inputId;
+    let request = new XMLHttpRequest();
+    request.open('DELETE', URL);
+    request.responseType = 'json';
+    request.onload = () => {
+      //render a homepage
+    }
+    request.send();
+  }
+
+  deposit = () => {
+    let URL = `/api/account/alter/` + this.state.id;
+    let request = new XMLHttpRequest();
+    request.open('PUT', URL);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Accept", "application/json");
+    request.responseType = 'json';
+    request.onload = () => {
+      this.getAcc();
+    }
+    let body = {
+      id: this.state.id,
+      balance: this.state.deposit
+    };
+    body = JSON.stringify(body);
+    request.send(body);
+  }
+
+  withdraw = () => {
+    let URL = `/api/account/alter/` + this.state.id;
+    let request = new XMLHttpRequest();
+    request.open('PUT', URL);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Accept", "application/json");
+    request.responseType = 'json';
+    request.onload = () => {
+      this.getAcc();
+    }
+    let body = {
+      id: this.state.id,
+      balance: -(this.state.withdraw)
+    };
+    body = JSON.stringify(body);
+    request.send(body);
+  }
+
+  check = () => {
+    let URL = `/api/account/check` + this.state.transferRecipient;
+    let request = new XMLHttpRequest();
+    request.open('GET', URL);
+    request.responseType = 'json';
+    request.onload = () => {
+      let exists = request.response;
+      if (exists === 1) {
+        this.transfer();
+      } else {
         this.setState({
-          id: request.response.id,
-          balance: request.response.balance
-        });
+          errorMsg: "User id doesnt exist"
+        })
       }
-      request.send();
     }
+    request.send();
+  }
 
-    createAcc = () => {
-      let URL = `/api/account`;
-      let request = new XMLHttpRequest();
-      request.open('POST', URL);
-      request.setRequestHeader("Content-Type", "application/json");
-      request.setRequestHeader("Accept", "application/json");
-      request.responseType = 'json';
-      request.onload = () => {
-        console.log(request.response)
-        // this.getAcc();
-      }
-      let body = {
-        balance: 0
-      };
-      body = JSON.stringify(body);
-      request.send(body);
+  transfer = () => {
+    let URL = `/api/account/transfer/` + this.state.transferRecipient;
+    let request = new XMLHttpRequest();
+    request.open('PUT', URL);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Accept", "application/json");
+    request.responseType = 'json';
+    request.onload = () => {
+      this.getAcc();
     }
+    let body = {
+      id: this.state.id,
+      balance: this.state.transferAmount
+    };
+    body = JSON.stringify(body);
+    request.send(body);
+  }
 
-    deleteAcc = () => {
-      let URL = `/api/account/` + this.state.inputId;
-      let request = new XMLHttpRequest();
-      request.open('DELETE', URL);
-      request.responseType = 'json';
-      request.onload = () => {
-        //render a homepage
-      }
-      request.send();
-    }
+  inputId = (d) => {
+    this.setState({
+      inputId: d.target.value
+    });
+  }
 
-    deposit = () => {
-      let URL = `/api/account/alter/` + this.state.id;
-      let request = new XMLHttpRequest();
-      request.open('PUT', URL);
-      request.setRequestHeader("Content-Type", "application/json");
-      request.setRequestHeader("Accept", "application/json");
-      request.responseType = 'json';
-      request.onload = () => {
-        this.getAcc();
-      }
-      let body = {
-        id: this.state.id,
-        balance: this.state.deposit
-      };
-      body = JSON.stringify(body);
-      request.send(body);
-    }
+  inputDeposit = (d) => {
+    this.setState({
+      deposit: d.target.value
+    });
+  }
 
-    withdraw = () => {
-      let URL = `/api/account/alter/` + this.state.id;
-      let request = new XMLHttpRequest();
-      request.open('PUT', URL);
-      request.setRequestHeader("Content-Type", "application/json");
-      request.setRequestHeader("Accept", "application/json");
-      request.responseType = 'json';
-      request.onload = () => {
-        this.getAcc();
-      }
-      let body = {
-        id: this.state.id,
-        balance: -(this.state.withdraw)
-      };
-      body = JSON.stringify(body);
-      request.send(body);
-    }
+  inputWithdraw = (d) => {
+    this.setState({
+      withdraw: d.target.value
+    });
+  }
 
-    check = () => {
-      let URL = `/api/account/check` + this.state.transferRecipient;
-      let request = new XMLHttpRequest();
-      request.open('GET', URL);
-      request.responseType = 'json';
-      request.onload = () => {
-        let exists = request.response;
-        if (exists === 1) {
-          this.transfer();
-        } else {
-          this.setState({
-            errorMsg: "User id doesnt exist"
-          })
-        }
-      }
-      request.send();
-    }
+  transferAmount = (d) => {
+    this.setState({
+      transferAmount: d.target.value
+    });
+  }
 
-    transfer = () => {
-      let URL = `/api/account/transfer/` + this.state.transferRecipient;
-      let request = new XMLHttpRequest();
-      request.open('PUT', URL);
-      request.setRequestHeader("Content-Type", "application/json");
-      request.setRequestHeader("Accept", "application/json");
-      request.responseType = 'json';
-      request.onload = () => {
-        this.getAcc();
-      }
-      let body = {
-        id: this.state.id,
-        balance: this.state.transferAmount
-      };
-      body = JSON.stringify(body);
-      request.send(body);
-    }
-
-    inputId = (d) => {
-      this.setState({
-        inputId: d.target.value
-      });
-    }
-
-    inputDeposit = (d) => {
-      this.setState({
-        deposit: d.target.value
-      });
-    }
-
-    inputWithdraw = (d) => {
-      this.setState({
-        withdraw: d.target.value
-      });
-    }
-
-    transferAmount = (d) => {
-      this.setState({
-        transferAmount: d.target.value
-      });
-    }
-
-    transferRecipient = (d) => {
-      this.setState({
-        transferRecipient: d.target.value
-      });
-    }
+  transferRecipient = (d) => {
+    this.setState({
+      transferRecipient: d.target.value
+    });
+  }
 }
